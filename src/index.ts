8b4722736e66d38fa2523734917c23d11f97351e
Ben A <ben.atkinson@lexicondigital.com.au>
2024-02-22 11:34:32 +1100

    feat: basic csv parsing script

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('hello');
+import fs from "node:fs";
+import path from "node:path";
+import { parse } from "csv-parse";
+// Note, the `stream/promises` module is only available
+// starting with Node.js version 16
+import { finished } from "stream/promises";
+
+const inputPath = path.join(__dirname, "input", "input.csv");
+const processFile = async () => {
+  const records: any[] = [];
+  const parser = fs.createReadStream(inputPath).pipe(
+    parse({
+      // CSV options if any
+    }),
+  );
+  parser.on("readable", function () {
+    let record;
+    while ((record = parser.read()) !== null) {
+      // Work with each record
+      records.push(record);
+    }
+  });
+  await finished(parser);
+  console.log(records);
+  return records;
+};
+// Parse the CSV content
+processFile();
