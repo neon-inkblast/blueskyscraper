8cd0a5af6fdead3a29fdbb2a76d3a1d45bd1b690
Ben A <ben.atkinson@lexicondigital.com.au>
2024-02-22 13:02:53 +1100

    feat: move example json

--- src/index.ts
+++ src/index.ts
@@ -4,50 +4,10 @@ import { processFile } from "./readInput";
 import { JSONData, writeOutput } from "./writeOutput";
 import { AllianzQuote } from "./competitors/allianz/types";
 
-// Example usage
-const jsonData: JSONData[] = [
-  {
-    duration: 30,
-    leadDays: 10,
-    excess: 0,
-    cancellationCover: 10000,
-    issueDate: "date",
-    img: 200,
-    safetyWing: 200,
-    allianz: 200,
-    battleface: 200,
-    travelGuard: 200,
-  },
-  {
-    duration: 30,
-    leadDays: 10,
-    excess: 100,
-    cancellationCover: 5000,
-    issueDate: "date",
-    img: 300,
-    safetyWing: 300,
-    allianz: 300,
-    battleface: 300,
-    travelGuard: 300,
-  },
-  {
-    duration: 30,
-    leadDays: 10,
-    excess: 150,
-    cancellationCover: 20000,
-    issueDate: "date",
-    img: 150,
-    safetyWing: 150,
-    allianz: 150,
-    battleface: 150,
-    travelGuard: 150,
-  },
-];
-
 const scrape = async () => {
   const records = await processFile();
   await getCompetitorPrices(records);
-  await writeOutput(jsonData);
+  await writeOutput();
 };
 
 const defaultHeaders = {
--- src/writeOutput.ts
+++ src/writeOutput.ts
@@ -1,15 +1,56 @@
-import { parse } from 'json2csv';
-import fs from 'fs';
+import { parse } from "json2csv";
+import fs from "fs";
 
 export interface JSONData {
   [key: string]: any;
 }
-export const writeOutput = async (input: JSONData[]) => {
+
+// Example usage
+const jsonData: JSONData[] = [
+  {
+    duration: 30,
+    leadDays: 10,
+    excess: 0,
+    cancellationCover: 10000,
+    issueDate: "date",
+    img: 200,
+    safetyWing: 200,
+    allianz: 200,
+    battleface: 200,
+    travelGuard: 200,
+  },
+  {
+    duration: 30,
+    leadDays: 10,
+    excess: 100,
+    cancellationCover: 5000,
+    issueDate: "date",
+    img: 300,
+    safetyWing: 300,
+    allianz: 300,
+    battleface: 300,
+    travelGuard: 300,
+  },
+  {
+    duration: 30,
+    leadDays: 10,
+    excess: 150,
+    cancellationCover: 20000,
+    issueDate: "date",
+    img: 150,
+    safetyWing: 150,
+    allianz: 150,
+    battleface: 150,
+    travelGuard: 150,
+  },
+];
+
+export const writeOutput = async (input: JSONData[] = jsonData) => {
   try {
     const csv = parse(input);
-    await fs.writeFileSync('src/output/output.csv', csv);
-    console.log('CSV file successfully created');
+    await fs.writeFileSync("src/output/output.csv", csv);
+    console.log("CSV file successfully created");
   } catch (err) {
-    console.error('Error converting JSON to CSV:', err);
+    console.error("Error converting JSON to CSV:", err);
   }
 };
