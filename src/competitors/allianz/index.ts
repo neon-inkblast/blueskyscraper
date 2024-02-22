b86dce1285d8ee61ce846394e0af9690922a425f
Pritchard <lee.pritchard@dynatrace.com>
2024-02-22 13:16:48 +1100

    Added some stuff to chose competitor get

--- src/competitors/allianz/index.ts
+++ src/competitors/allianz/index.ts
@@ -3,6 +3,10 @@ import { extractQuoteInfo } from "../..";
 
 export const ALLIANZ = {
   requiresAuth: true,
+  requestor: function(){
+    console.log("ha");
+    getCompetitorPrices();
+  },
   endpoints: {
     auth: {
       url: "https://allianzassistancetravel.com.au/onex/api/Application/Authenticate",
@@ -52,4 +56,48 @@ function allianzQuoteOutputAdapter(input: AllianzQuote) {
   return input.entities[0].entities.map((entity) => {
     return entity.properties.price.sellingPrice;
   });
+}
+
+
+
+const defaultHeaders = {
+  "Content-Type": "application/json",
+  "User-Agent":
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+};
+async function getCompetitorPrices(records: any[]) {
+  const competitorScrapePromises = Object.entries(competitors).map(async ([k, v]) => {
+    console.log("processing competitor: ", k);
+    if (v.requiresAuth) {
+      let token: string;
+      try {
+        const authResult = await axios.post(v.endpoints.auth.url, v.endpoints.auth.body, {
+          headers: { ...defaultHeaders },
+        });
+        token = v.authTokenAdapter(authResult);
+      } catch (e) {
+        console.error("auth error:", e);
+        throw e;
+      }
+
+      if (!token) {
+        throw Error("Missing auth token!");
+      }
+
+      // uncomment this loop to run once for each record instead of just once with dummy data
+      // records.forEach(async (record: any) => {
+      try {
+        const quoteRequestBody = v.inputAdapter({});
+        const quote = await axios.post(v.endpoints.quote.url, quoteRequestBody, {
+          headers: { ...defaultHeaders, Authorization: `Bearer ${token}` },
+        });
+        const prices = v.quoteAdapter(quote.data as AllianzQuote);
+        console.log(prices);
+      } catch (e) {
+        console.error("quote error:", e, "for record", records);
+      }
+      // });
+    }
+  });
+  await Promise.allSettled(competitorScrapePromises);
 }
--- src/index.ts
+++ src/index.ts
@@ -33,9 +33,60 @@ export const extractQuoteInfo = (record: any) => {
   return quoteInfo;
 }
 
+
+
+/*
+{
+      url: "https://allianzassistancetravel.com.au/onex/api/quick-quote/travel",
+      body: JSON.stringify({
+        destinationIds: ["DEU", "ESP"],
+        startDate: "2024-02-24",
+        endDate: "2024-02-26",
+        ageOfAdults: [22, 24],
+        ageOfDependants: [],
+        answeredQuestions: [{ id: "RESID", answer: { id: "Y" } }],
+      }),
+      method: "POST",
+    }
+
+    */
+
 const scrape = async () => {
   const records = await processFile();
-  await getCompetitorPrices(records);
+
+  let output = {};
+
+  let first = true;
+  let requestedCompetitors : any = [];
+  let compAdd = false;
+
+  // console.log(records[0]);
+
+  records[0].forEach((record) => {
+    if(compAdd){
+      requestedCompetitors.push(record);
+    }
+    if(record == "Timestamp (issue date)"){
+      compAdd = true;
+    }
+  });
+
+    console.log("requestedCompetitors");
+    console.log(requestedCompetitors);
+
+    console.log("competitors");
+    console.log(competitors);
+
+
+    requestedCompetitors.forEach(function(request){
+      if(!!competitors[request]){
+        competitors[request].requestor();
+      }
+    })
+
+
+
+
   await writeOutput();
 };
 
@@ -60,27 +111,5 @@ async function getCompetitorPrices(records: any[]) {
         console.error("auth error:", e);
         throw e;
       }
-
-      if (!token) {
-        throw Error("Missing auth token!");
-      }
-
-      // uncomment this loop to run once for each record instead of just once with dummy data
-      // records.forEach(async (record: any) => {
-      try {
-        const quoteRequestBody = v.inputAdapter({});
-        const quote = await axios.post(v.endpoints.quote.url, quoteRequestBody, {
-          headers: { ...defaultHeaders, Authorization: `Bearer ${token}` },
-        });
-        const prices = v.quoteAdapter(quote.data as AllianzQuote);
-        console.log(prices);
-      } catch (e) {
-        console.error("quote error:", e, "for record", records);
-      }
-      // });
-    }
-  });
-  await Promise.allSettled(competitorScrapePromises);
-}
 
 scrape();
