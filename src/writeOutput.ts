4842df7c3eb889b06bc971d9a7b020e68cdcf62c
izabelawojciak <izabela.wojciak@endava.com>
2024-02-22 12:54:04 +1100

    writeOutput function implemented.

--- src/writeOutput.ts
+++ src/writeOutput.ts
@@ -1,3 +1,15 @@
-export const writeOutput = async () => {
-  await console.log("writing output");
+import { parse } from 'json2csv';
+import fs from 'fs';
+
+export interface JSONData {
+  [key: string]: any;
+}
+export const writeOutput = async (input: JSONData[]) => {
+  try {
+    const csv = parse(input);
+    await fs.writeFileSync('src/output/output.csv', csv);
+    console.log('CSV file successfully created');
+  } catch (err) {
+    console.error('Error converting JSON to CSV:', err);
+  }
 };
